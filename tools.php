7bfe347a6283b34a79313f9f2c210e9d807de19d
mirkolofio <mirkolofio@gmail.com>
2011-11-09 14:48:58 +0100

    tools

--- tools.php
+++ tools.php
@@ -1,7 +1,28 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+<?
+/**
+ * @param String $dir
+ * @return bool
+ *
+ * @author holger1@zentralplan.de
+ * @link http://php.net/manual/en/function.rmdir.php#98622
  */
+function rrmdir ($dir)
+{
+	if (is_dir ($dir))
+	{
+		$objects = scandir ($dir);
+		foreach ($objects as $object)
+			if ($object != "." && $object != "..")
+				if (filetype ($dir . "/" . $object) == "dir")
+					rrmdir ($dir . "/" . $object);
+				else
+					unlink ($dir . "/" . $object);
 
-?>
+		reset ($objects);
+		rmdir ($dir);
+
+		return true;
+	}
+
+	return false;
+}
