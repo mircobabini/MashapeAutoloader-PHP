ad478896ac217fed889e006176c73f8a54dd6930
mirkolofio <mirkolofio@gmail.com>
2011-11-09 10:14:03 +0100

    just a sample

---
+++ sample.php
@@ -0,0 +1,18 @@
+<?
+require_once "MashapeAutoloader.php";
+
+// you can find these on mashape.com
+$publicKey = "your-public-key";
+$privateKey = "your-private-key";
+
+
+// set local store dir
+MashapeAutoloader::store ("../../data/mashape-apis/");
+
+// first way
+$json = MashapeAutoloader::Unshortener ($publicKey, $privateKey)->unshort ("http://wp.me/p1e4Gf-6r");
+
+// second way: compatibility version
+MashapeAutoloader::auth ($publicKey, $privateKey);
+$json2 = MashapeAutoloader::exec ("Unshortener", "unshort", "http://wp.me/p1e4Gf-6r");
+
